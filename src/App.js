e0ae34c0d9e28cb00316093fba3c53d12c51bd55
Matei Radu <matei.radu.92@gmail.com>
2018-05-08 22:15:16 +0200

    Bootstraps project with create-react-app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+/*
+ * Progressive Weather
+ * Copyright © 2018 Matei Bogdan Radu <matei.radu.92@gmail.com>
+ * Licensed under MIT
+ * (https://github.com/matt-block/progressive-weather/blob/master/LICENSE)
+ */
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
+        </header>
+        <p className='App-intro'>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,14 @@
+/*
+ * Progressive Weather
+ * Copyright © 2018 Matei Bogdan Radu <matei.radu.92@gmail.com>
+ * Licensed under MIT
+ * (https://github.com/matt-block/progressive-weather/blob/master/LICENSE)
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
