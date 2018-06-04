e3aa4cee70d04b923a09084a3d1e447aa2e57709
Matei Radu <matei.radu.92@gmail.com>
2018-06-04 21:40:20 +0200

    Adds SettingsIcon

---
+++ src/Icons/SettingsIcon.jsx
@@ -0,0 +1,13 @@
+/*
+ * Progressive Weather
+ * Copyright © 2018 Matei Bogdan Radu <matei.radu.92@gmail.com>
+ * Licensed under MIT
+ * (https://github.com/matt-block/progressive-weather/blob/master/LICENSE)
+ */
+
+import React from 'react'
+import icon from '../assets/ic_settings.svg'
+
+export function SettingsIcon() {
+  return <img className='icon-settings' src={icon} alt=''/>
+}
