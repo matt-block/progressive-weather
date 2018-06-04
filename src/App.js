dd928418d740f81db34003e335ca49112a9942b0
Matei Radu <matei.radu.92@gmail.com>
2018-06-04 22:08:49 +0200

    Adds React Router

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@
 
 import React from 'react'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { store } from './store'
 import CurrentWeather from './CurrentWeather'
 import Forecast from './Forecast'
@@ -16,11 +17,13 @@ import './App.css'
 function App() {
   return (
     <Provider store={store}>
-      <React.Fragment>
-        <Toolbar />
-        <CurrentWeather />
-        <Forecast />
-      </React.Fragment>
+      <BrowserRouter>
+        <React.Fragment>
+          <Route path='/' component={Toolbar} />
+          <Route path='/' component={CurrentWeather} />
+          <Route path='/' component={Forecast} />
+        </React.Fragment>
+      </BrowserRouter>
     </Provider>
   )
 }
