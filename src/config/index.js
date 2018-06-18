9d13290fd8e27b53b0f6693901aa5cc304d2fb0f
Matei Radu <matei.radu.92@gmail.com>
2018-06-18 22:00:34 +0200

    Release v1.5.2

--- src/config/index.js
+++ src/config/index.js
@@ -24,7 +24,7 @@ export const API_URL = 'https://openweathermap.org/'
  *
  * @see {@link https://semver.org/}
  */
-export const APP_VERSION = '1.5.1'
+export const APP_VERSION = '1.5.2'
 
 /**
  * URL of the application repository.
